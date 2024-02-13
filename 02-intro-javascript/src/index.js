eb17bebdbce0cf489ba8550ae956f341441291c8
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-13 16:14:32 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -3,6 +3,10 @@ const person = {
     firstName: 'Tony',
     lastName: 'Melony',
     age: 40,
+    address:{
+        city:'Chihuahua',
+        zip: '3105',
+    }
 };
 
 console.log(person.firstName);
