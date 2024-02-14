b44522269376cb360b25329cdceb31ea5ca3b654
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-14 16:17:37 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,6 +1,12 @@
 // console.log('What is happening here?');
-//functions
-function sayHi(firstName){
+//functions traditional way is not recommendable because we can re asign with another thing
+// function sayHi(firstName){
+//     return `Hi ${firstName}`;
+// }
+// sayHi = 30;
+// better idea, use a const
+
+const sayHi = function (firstName){
     return `Hi ${firstName}`;
 }
 
