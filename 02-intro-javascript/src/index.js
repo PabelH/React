585d3ea0e53449569b094a0f3d54cbf95335bf9e
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-19 16:07:52 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,14 +1,22 @@
 // console.log('What is happening here?');
 //destructuring
 const person = {
-    name: 'Pabelon',
+    name1: 'Pabelon',
     age: 30,
     key:123,
 };
 
 // extract this information from this object
-const {name} = person;
+// const {name} = person;
 
-console.log(person.name);
+// console.log(person.name);
 // console.log(person.age);
 // console.log(person.key);
+
+//if I need rename the prop.
+// const {name:name2} = person;
+
+const {name1, age, key} = person;
+console.log(name1);
+console.log(age);
+console.log(key);
