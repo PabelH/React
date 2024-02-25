7f49f496f9c8ab2d73b7851029d310cddc22a99d
Pabel Hernández <pabel.hernandez@live.com>
2024-02-24 18:35:51 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -5,9 +5,15 @@ import { heroes } from './data/heroes';
 
 // heroes you can just write the reference you need and it will be imported
 
-const getHeroById = (id) =>{
-    return heroes.find((heroes) => heroes.id === id);
-
-}
+// const getHeroById = (id) =>{
+//     return heroes.find((heroes) => heroes.id === id);
+// } same thing less code...v
+const getHeroById = (id) => heroes.find((heroes) => heroes.id === id);
 
 console.log(getHeroById(2));
+// .find -> .filter
+// const getHeroByOwner = (owner) => heroes.filter((heroes) => heroes.id === id);
+
+// console.log(getHeroByOwner('DC'));
+
+
