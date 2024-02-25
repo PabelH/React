b641786dcd1d764166047eb2d755050b84e68a10
Pabel Hernández <pabel.hernandez@live.com>
2024-02-25 14:42:27 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,2 +1,15 @@
 // console.log('What is happening here?');
 
+import { getHeroeById } from './bases/08-imp-exp'
+
+// const promesa = new Promise( (resolve, reject) => {
+
+//     setTimeout( () =>  {
+//         // Tarea
+//         // importen el 
+//         const p1 = getHeroeById(2);
+//         resolve( p1 );
+//         // reject( 'No se pudo encontrar el héroe' );
+//     }, 2000 )
+
+// });
