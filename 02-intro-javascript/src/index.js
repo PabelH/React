177c61890386394b72534d4ce02f154795d7f085
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-26 14:46:54 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,9 +1,9 @@
 // console.log('What is happening here?');
 //Promise
-
+// this code run 5 seconbds after
 const prom1 = new Promise((resolve,reject) =>{
     setTimeout( () => {
-        console.log('5 segundos despues')
-    }, 5000 )
+        console.log('2 segundos despues')
+    }, 2000 )
 
 });
