29e7fa9031936b92315feeeac3bca774dab4eefb
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-27 13:18:40 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -4,17 +4,32 @@
 import { getHeroById } from './bases/08-imp-exp';
 
 // this code run 5 seconbds after
-const prom1 = new Promise((resolve,reject) =>{
-    setTimeout( () => {
-        const hero = getHeroById(2);
-        // console.log(hero);
-        resolve(hero);
-    }, 2000 )
+// const prom1 = new Promise((resolve,reject) =>{
+//     setTimeout( () => {
+//         const hero = getHeroById(2);
+//         // console.log(hero);
+//         resolve(hero);
+//     }, 2000 )
     
 
-});
+// });
 
-prom1.then( (hero) => {
-    console.log('Hero', hero);
-} )
-.catch( err => console.warn('We can\'t find the hero'));
+// prom1.then( (hero) => {
+//     console.log('Hero', hero);
+// } )
+// .catch( err => console.warn('We can\'t find the hero'));
+
+const getHeroByIdAsync = ( id ) => {
+    const prom1 = new Promise((resolve,reject) =>{
+        setTimeout( () => {
+            const hero = getHeroById(id);
+            // console.log(hero);
+            resolve(hero);
+        }, 2000 )
+        
+    
+    });
+}
+
+
+
