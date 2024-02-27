07675a8bd2ef274bee451afa8245de472a7cd5ea
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-27 13:47:18 -0600

    update

---
+++ 02-intro-javascript/src/bases/09-promises.js
@@ -0,0 +1,46 @@
+// console.log('What is happening here?');
+//Promise
+
+import { getHeroById } from './bases/08-imp-exp';
+
+// this code run 5 seconbds after
+// const prom1 = new Promise((resolve,reject) =>{
+//     setTimeout( () => {
+//         const hero = getHeroById(2);
+//         // console.log(hero);
+//         resolve(hero);
+//     }, 2000 )
+    
+
+// });
+
+// prom1.then( (hero) => {
+//     console.log('Hero', hero);
+// } )
+// .catch( err => console.warn('We can\'t find the hero'));
+
+const getHeroByIdAsync = ( id ) => {
+    
+    return new Promise((resolve,reject) =>{
+        setTimeout( () => {
+            const hero = getHeroById(id);
+            // console.log(hero);
+            if (hero) {
+            resolve(hero);
+            } else {
+            reject('We can\'t find the hero');
+            }
+        }, 2000 )
+        
+    
+    });
+}
+
+getHeroByIdAsync(1)
+    // .then(hero => console.log('Hero', hero))
+    // .catch( err => console.error('We can\'t find the hero'));
+    // if you only want to send the first argument 
+    .then(console.log)
+    .catch(console.warn);
+
+
