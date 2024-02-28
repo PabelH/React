380c6415d458cea0cb629a1231a03ac950f4e2bb
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-28 15:07:29 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -8,3 +8,17 @@ const getImg = async() => {
 }
 
 getImg().then(console.log);
+
+
+// const apiKey = 'your apikey';
+
+// const request = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+// request
+//     .then(resp => resp.json())
+//     .then(({data}) =>{
+//         const {url} = data.images.original;
+//         const img = document.createElement('img');
+//         img.src = url;
+//         document.body.append(img);
+//     })
+//     .catch(console.warn);
