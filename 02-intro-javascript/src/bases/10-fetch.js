73b3700fa338752fa399f0d01f2bcf399bf84eef
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-28 14:47:44 -0600

    update

---
+++ 02-intro-javascript/src/bases/10-fetch.js
@@ -0,0 +1,33 @@
+// console.log('What is happening here?');
+const apiKey = 'your apikey';
+
+const request = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+//ugly and unsustainable code
+// request.then(resp =>{
+//     resp.json().then (data =>{
+//         console.log(data);
+//     });
+// })
+// .catch(console.warn);
+
+//chain promises
+// request
+//     .then(resp => resp.json())
+//     .then(({data}) =>{
+//         const {url} = data.images.original;
+//         console.log(url)
+//     })
+//     .catch(console.warn);
+
+
+// show in HTML the image
+request
+    .then(resp => resp.json())
+    .then(({data}) =>{
+        const {url} = data.images.original;
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+    })
+    .catch(console.warn);
