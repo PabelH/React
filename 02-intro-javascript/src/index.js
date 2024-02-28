1bf69c59c9245c29eccbed2469d4589fdcda55dc
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-28 14:58:17 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,4 +1,6 @@
 // console.log('What is happening here?');
-const getImageProm = () => {
-    
-}
+//using promises
+const getImageProm = () => new Promise( resolve => resolve('https://dsafñjnfñklasdmfñlka'));
+
+getImageProm().then(console.log);
+
