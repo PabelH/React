e7a5cf9b1a75e1ed10dfeca9dc713a3bc438c7f4
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-02-29 15:13:53 -0600

    update

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,2 +1,15 @@
 // console.log('What is happening here?');
+// ternary conditional operator
+const active = true;
 
+// let message = '';
+// if (!active) {
+//     message = '=)';
+// } else {
+//     message = '=(';
+// }
+//better way to do 
+
+
+
+console.log(message);
