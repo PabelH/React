349275c830be40319d685dc121286363d05c0153
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-03-12 14:45:32 -0600

    update

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./FirstApp";
 
-function App() {
-    return <h1>Hello Pabello</h1>
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
