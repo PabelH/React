699e74974eb48e5f0a153ec84e370aaa3bcb15a3
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-03-12 15:00:26 -0600

    update

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,5 +1,20 @@
-function App() {
-    return <h1>Hello Pabello</h1>
+// function App() {
+//     return <h1>Hello Pabello</h1>
+// }
+
+//using rafc
+// import React from 'react'
+
+const FirstApp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default FirstApp
+
+
+
+// export default App;
