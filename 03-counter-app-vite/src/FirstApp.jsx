190c498a09438efb47882f9a40dd9268a2bd6875
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-03-14 14:35:56 -0600

    update

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -2,13 +2,18 @@
 // <Fragment></Fragment> === <></>
 //you can render JS expressions as long as it is not an object
 //if you don't need to render again
-const newMessage = 'Pabelon';
+// const newMessage = 'Pabelon';
+const newMessage = {
+    message: 'Hi there',
+    author: 'Pabelon'
+};
 
 const FirstApp = () => {
     
   return (
     <> 
-    <h1> { newMessage } </h1>
+    {/* <h1> { newMessage } </h1>  If the variable is an object this is not allowed*/}
+    <h1> { newMessage.author } </h1>
     <h2>I am Learning</h2>
     
     </>
