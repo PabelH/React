6512d78588d8470fa4d487309e6fae3f61c06a60
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-05-02 15:27:38 -0600

    update

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,12 +1,16 @@
 import PropTypes  from "prop-types"
 
 const CounterApp = ({value}) => {
+
+  const handleAdd = (event) => console.log(event);
+
+
   return (
     <>
     <h1>CounterApp</h1>
     <h2> { value } </h2>
 
-    <button onClick={ function() {console.log('Hello Pabello')} }>
+    <button onClick={handleAdd }>
       +1
     </button>
     </>
