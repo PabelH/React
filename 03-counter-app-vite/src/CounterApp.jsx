d5a29ff63b61d657571949255a1405a71019943b
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-05-02 14:48:00 -0600

    update

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -6,7 +6,7 @@ const CounterApp = ({value}) => {
     <h1>CounterApp</h1>
     <h2> { value } </h2>
 
-    <button>
+    <button onClick={ function() {console.log('Hello Pabello')} }>
       +1
     </button>
     </>
