938c97f53333ef903b7cecd6eed79d19761d3b74
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-05-06 10:15:36 -0600

    update

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react';
 import PropTypes  from "prop-types"
 
 const CounterApp = ({ value }) => {
 
+  const [ counter, setCounter ] = useState( 0 );
+
   const handleAdd = () => {
-    console.log('+1');
-    value = 1000;
+    setCounter( counter + 1 )
   
   }
 
   return (
     <>
     <h1>CounterApp</h1>
-    <h2> { value } </h2>
+    <h2> { counter } </h2>
 
     <button onClick={ handleAdd }>
       +1
