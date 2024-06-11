14c70e6e114949eac4deeffb1cdf2b00d9e7aeea
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-06-11 14:09:04 -0600

    update

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -14,6 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         {/* <FirstApp /> */}
         {/* <App /> */}
 
-        <CounterApp value={5} />
+        <CounterApp value={0} />
     </React.StrictMode>
 )
