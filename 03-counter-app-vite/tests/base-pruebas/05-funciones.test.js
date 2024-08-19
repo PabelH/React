9f335818ab78cdbb9b0261ff279799a7fbad085f
Pabel Hernández <pabel.hernandez@live.com>
2024-08-19 05:29:21 -0600

    update

---
+++ 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,9 @@
+describe('Test in 05-funciones', () => {
+  test('this test should return an object',()=>{
+      const testUser = {
+        uid: 'ABC123',
+        username: 'El_Papi1502'
+}
+  });
+
+});
