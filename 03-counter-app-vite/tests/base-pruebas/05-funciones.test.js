c25f1656adbacfdc5a9507c0ecc15a869621c48a
Pabel Hernández <pabel.hernandez@live.com>
2024-08-30 05:41:42 -0600

    update

--- 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
+++ 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -1,18 +1,34 @@
-import { getUser } from "../../src/base-pruebas/05-funciones";
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
 
 describe('Test in 05-funciones', () => {
-  test('this test should return an object',()=>{
-      const testUser = {
-        uid: 'ABC123',
-        username: 'El_Papi1502'
-      };
+  test('this test should return an object', () => {
+    const testUser = {
+      uid: 'ABC123',
+      username: 'El_Papi1502'
+    };
 
-      const user = getUser();
-      
-      // expect( testUser ).toStrictEqual( user );
-      expect( testUser ).toEqual( user );
+    const user = getUser();
+
+    // expect( testUser ).toStrictEqual( user );
+    expect(testUser).toEqual(user);
 
 
   });
 
+  test('this test should return an object', () => {
+
+    const name = 'Pabelon'
+
+    const user = getUsuarioActivo(name);
+    
+    expect(user).toStrictEqual({
+      uid: 'ABC567',
+      username: name
+    });
+
+
+  });
+
+
 });
+
