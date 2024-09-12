fe88e565e16e5f77493536a8dc05cc4793ba3801
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-09-12 15:02:35 -0600

    update

--- 03-counter-app-vite/tests/base-pruebas/08-imp-exp.test.js
+++ 03-counter-app-vite/tests/base-pruebas/08-imp-exp.test.js
@@ -1,3 +1,4 @@
+import { getHeroById, getHeroByOwner } from "../../../02-intro-javascript/src/bases/08-imp-exp";
 
 
 
@@ -5,8 +6,44 @@ describe('Test in 08-imp-exp', () => {
     test('this test should return a hero by ID (getHeroById)',()=>{
 
         const id = 1;
-        const hero =getHer
-        
+        const hero = getHeroById( id );
+        //console.log(hero);
+        expect( hero ).toEqual( { id: 1, name: 'Batman', owner: 'DC' } )
+
     });
+    test('this test should return undefind if there is no an ID  (getHeroById)',()=>{
+
+        const id = 100;
+        const hero = getHeroById( id );
+        //console.log(hero);
+        //another way
+        //expect( hero ).toBe( undefined )
+        expect( hero ).toBeFalsy(); //Be careful because false is also included
+
+    });
+
+    //HW getHeroByOwner
+    test('this test should return undefind if there is no an ID  (getHeroById)',()=>{
+
+        const owner = 'DC';
+        const hero = getHeroByOwner( owner );
+        console.log( hero );
+        expect( hero ).toEqual( [
+            { id: 1, name: 'Batman', owner: 'DC' },
+            { id: 3, name: 'Superman', owner: 'DC' },
+            { id: 4, name: 'Flash', owner: 'DC' }
+          ] )
+
+    });
+    test('this test should return undefind if there is no an ID  (getHeroById)',()=>{
+
+        const owner = 'DC';
+        const hero = getHeroByOwner( owner );
+        console.log( hero.length );
+        expect( hero.length ).toBe( 3 )
+
+    });
+
+    
   
 });
