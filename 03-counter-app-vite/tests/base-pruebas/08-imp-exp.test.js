1873b3992010cc03076f4f2c735db6635a05acf2
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-09-12 07:54:58 -0600

    update

---
+++ 03-counter-app-vite/tests/base-pruebas/08-imp-exp.test.js
@@ -0,0 +1,12 @@
+
+
+
+describe('Test in 08-imp-exp', () => {
+    test('this test should return a hero by ID (getHeroById)',()=>{
+
+        const id = 1;
+        const hero =getHer
+        
+    });
+  
+});
