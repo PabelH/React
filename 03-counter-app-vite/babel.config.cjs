1a1bb9e835d9bfc0bba3de6d07570ebf14033c5a
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-09-17 13:33:32 -0600

    update

--- 03-counter-app-vite/babel.config.cjs
+++ 03-counter-app-vite/babel.config.cjs
@@ -5,5 +5,10 @@
 // export const plugins = [];
 
 module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+    // presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+    presets:[
+      ['@babel/preset-env', {targets: {esmodules: true}}],
+      ['@babel/preset-react', {runtime: 'automatic'}],
+
+    ]
   };
--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -1,7 +1,13 @@
+import { render } from "@testing-library/react";
+import FirstApp from "../src/FirstApp";
 
 
 describe('Test in <FirstApp />', () => {
     test('this test must match the snapshot"',()=>{
+
+        const title = 'Hi MF'
+        render(<FirstApp title={ title }/>)
+        
         
     });
   
