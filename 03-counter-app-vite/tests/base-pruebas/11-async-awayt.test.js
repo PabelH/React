4a9be81a65fb48fd25a9898a1ea23a6046c05f22
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-09-17 12:35:35 -0600

    update

--- 03-counter-app-vite/tests/base-pruebas/11-async-awayt.test.js
+++ 03-counter-app-vite/tests/base-pruebas/11-async-awayt.test.js
@@ -2,11 +2,20 @@ import { getImg } from "../../../02-intro-javascript/src/bases/11-async-awayt";
 
 
 describe('Test in 11-async-awayt', () => {
-    test('this test should return an URL',async()=>{
+    // test('this test should return an URL',async()=>{
 
-        const url = await getImg();
-        console.log(url);
-        expect( typeof url ).toBe( 'string' )
+    //     const url = await getImg();
+    //     console.log(url);
+    //     expect( typeof url ).toBe( 'string' )
+        
+    // });
+
+    //Catch >>
+    test('this test should return an error if there is no an apiKey',async()=>{
+
+        const resp = await getImg();
+        console.log(resp);
+        expect( resp ).toBe( 'Image not found' )
         
     });
   
