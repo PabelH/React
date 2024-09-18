7fd0a856f8430be69a6a5fe37f4afcb37be2427f
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-09-18 10:11:51 -0600

    update

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -16,9 +16,12 @@ describe('Test in <FirstApp />', () => {
     test('this test must render the title in a h1 tag"',()=>{
 
         const title = 'Hi MF'
-        const {container} = render(<FirstApp title={ title }/>);
-        //console.log(container);
-        expect( container ).toMatchSnapshot();
+        const {container, getByText} = render(<FirstApp title={ title }/>);
+        expect( getByText(title) ).toBeTruthy();
+        const h1 = container.querySelector('h1');
+        // expect( h1.innerHTML ).toBe(title);
+        expect( h1.innerHTML ).toContain(title);
+
 
 
         
