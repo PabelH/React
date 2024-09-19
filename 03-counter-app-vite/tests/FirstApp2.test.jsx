3d370ca920391b4b1d256b43b47fc5b0999b09c6
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-09-19 15:07:22 -0600

    update

--- 03-counter-app-vite/tests/FirstApp2.test.jsx
+++ 03-counter-app-vite/tests/FirstApp2.test.jsx
@@ -3,28 +3,6 @@ import FirstApp from "../src/FirstApp";
 
 
 describe('Test in <FirstApp />', () => {
-    // test('this test must match the snapshot"',()=>{
-
-    //     const title = 'Hi MF'
-    //     const {container} = render(<FirstApp title={ title }/>);
-    //     //console.log(container);
-    //     expect( container ).toMatchSnapshot();
-
-
-        
-    // });
-    test('this test must render the title in a h1 tag"',()=>{
-
-        const title = 'Hi MF';
-        const subTitle = 'No Subtitle';
-        const {getByText, getAllByText } = render(<FirstApp title={ title }/>);
-        
-        // expect( getByText(subTitle) ).toBeTruthy();
-        //if you need more elements
-        expect( getAllByText(subTitle).length ).toBe(2);
-
-
-
-    });
+    
   
 });
