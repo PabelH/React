e51d4db74c9c246964c76d9587155e6ccaa59caa
Pabel Hernandez <105995600+PabelH@users.noreply.github.com>
2024-09-26 12:03:55 -0600

    update

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -4,12 +4,24 @@ import { useState } from "react"
 
 const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState([ 'Fenomenoide' ])
+  const [categories, setCategories] = useState([ 'Fenomenoide', 'Animaniacs' ])
 
   return (
     <>
     
+      {/* Title */}
       <h1>GifExpertApp</h1>
+      {/*Input  */}
+
+      {/*Gif List  */}
+      <ol>
+          {
+          categories.map( category => {
+              return <li key={ category }>{category}</li>
+          } )
+          
+          }
+      </ol>
       
     </>
   )
